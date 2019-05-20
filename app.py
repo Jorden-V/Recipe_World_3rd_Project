300ed6afe6e4de71f088dd34870b2f2edeb8fd0d
Jorden <jordenkv@gmail.com>
2019-05-20 14:17:51 +0000

    setup new category and cusine function so users can add new ones if they do not already exist

--- app.py
+++ app.py
@@ -19,6 +19,24 @@ def index():
 def add_recipe():
     return render_template('addrecipe.html', recipes=mongo.db.recipes.find(), categories=mongo.db.categories.find(), cuisine=mongo.db.cuisine.find(), serving_size=mongo.db.serving_size.find(), difficulty=mongo.db.difficulty.find() )
 
+@app.route('/add_new_category', methods=['POST'])
+def add_new_category():
+    category_name = mongo.db.categories
+    category_name.insert_one(request.form.to_dict())
+    return redirect(url_for('add_recipe'))
+    
+@app.route('/add_new_cuisine', methods=['POST'])
+def add_new_cuisine():
+    cuisine_type = mongo.db.cuisine
+    cuisine_type.insert_one(request.form.to_dict())
+    return redirect(url_for('add_recipe'))
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
