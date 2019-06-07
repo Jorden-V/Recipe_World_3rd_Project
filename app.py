ee10ccd9e8da54806cf03576fe29ddb4e23648c1
Jorden <jordenkv@gmail.com>
2019-06-07 15:37:06 +0000

    Commit to confirm full functionality once deployed

--- app.py
+++ app.py
@@ -278,4 +278,4 @@ def date_added():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP',),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
